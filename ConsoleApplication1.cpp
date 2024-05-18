1a1edce95e43b1c48d76c2af90e5b79ebeb36cc5
reuss1te <95479217+reuss1te@users.noreply.github.com>
2024-05-18 11:46:17 +0400

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <vector>
+#include <iostream>
+
+
+std::vector<int> increaseVectorElementsByN(const std::vector<int>& vec, int n) {
+    std::vector<int> result;
+
+    for (int num : vec) {
+        result.push_back(num + n);
+    }
+
+    return result;
+}
+
+int main() {
+    std::vector<int> numbers = { 1, 2, 3, 4 };
+    std::vector<int> increasedNumbers = increaseVectorElementsByN(numbers, 5);
+    for (int num : increasedNumbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
