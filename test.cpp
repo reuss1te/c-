b51394111f73155097f26bf882a7f63f04605794
reuss1te <95479217+reuss1te@users.noreply.github.com>
2024-05-18 11:47:09 +0400

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,14 @@
+﻿#include "gtest/gtest.h"
+
+TEST(VectorOperationsTest, IncreaseVectorElementsByN) {
+    std::vector<int> numbers = { 1, 2, 3, 4 };
+    std::vector<int> expectedResult = { 6, 7, 8, 9 };
+    std::vector<int> actualResult = increaseVectorElementsByN(numbers, 5);
+
+    EXPECT_EQ(actualResult, expectedResult);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
